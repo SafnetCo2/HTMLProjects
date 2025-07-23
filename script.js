4a0a9a3115489efae97f7e495583719e5b9f6c2f
Jospine <nzi19001@byui.edu>
2025-07-23 21:13:15 +0300

    modified file to submit to google script

--- script.js
+++ script.js
@@ -13,9 +13,33 @@ document.addEventListener("DOMContentLoaded", function () {
             return;
         }
         //simulate form submission
-        alert("Thank you" + name + "!message sent");
+        //alert("Thank you" + name + "!message sent");
         //reset form
-        form.reset();
+        //form.reset();
+        //scripts for  google script
+        const scriptURL = "https://script.google.com/macros/s/AKfycbx_2-14sQ16kLK-PNTVf97_isndBGXl9vJ950jOZlG7qOjAVqoYaH7RiyhpwSkWylrN7Q/exec"
+
+        fetch(scriptURL, {
+            method: "POST",
+            body: JSON.stringify({
+                name, email, subject, message
+
+            }),
+            headers: {
+                "content-Type": "<application /json"
+            }
+        })
+            .then(response => response.json())
+            .then(data => {
+                alert("message sent and saved")
+                form.reset();
+            })
+            .catch(error => {
+                console.error("Error!", error.message);
+                alert("please try again")
+            });
+
 
     });
 });
+
