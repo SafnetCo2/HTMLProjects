ec7c549f22bbc186379aebb9636bea75b207107c
Jospine <nzi19001@byui.edu>
2025-07-30 13:33:02 +0300

    script for g.docs

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //https://script.google.com/macros/s/AKfycbzhk5ByfrO04UxXyPs5DJekQ__zR8UN0JlFHCGDvJFNGkW0bGC1cno3_7IFKBp2jfCA0w/exec
-    
-const scriptURL ='https://script.google.com/macros/s/AKfycbzhk5ByfrO04UxXyPs5DJekQ__zR8UN0JlFHCGDvJFNGkW0bGC1cno3_7IFKBp2jfCA0w/exec'
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbw9Yngflwy8m0mAZjbu5eBrzlEp5J5BJCzzBJc5aUww9WoxAV7vgzXUDTMPsEM3BlTUbw/exec'
 const form = document.forms['submit-to-google-sheet']
 
 form.addEventListener('submit', e => {
@@ -14,14 +14,14 @@ form.addEventListener('submit', e => {
 
 
 
-    // Optional: update date on the page
-    const updatedDate = new Date().toLocaleDateString('en-US', {
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric'
-    });
-    const lastUpdated = document.getElementById('last-updated');
-    if (lastUpdated) {
-        lastUpdated.textContent = updatedDate;
-    }
+// Optional: update date on the page
+const updatedDate = new Date().toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+});
+const lastUpdated = document.getElementById('last-updated');
+if (lastUpdated) {
+    lastUpdated.textContent = updatedDate;
+}
 
